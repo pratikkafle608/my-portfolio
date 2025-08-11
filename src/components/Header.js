3707f37d955adc1c944e6a8130305ea83109353d
Pratik Kafle <123042495+pratikkafle608@users.noreply.github.com>
2025-08-11 05:33:54 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
         <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
         <a href="#experience">Experience</a>
-        <a href="#contact">Contact</a>
+    
       </nav>
     </header>
   );
